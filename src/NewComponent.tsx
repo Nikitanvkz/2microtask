a723ae95095579d0cad053425dc9482a21cb3cae
Nikitanvkz <nikitasharafutdinov@yandex.ru>
2022-05-26 23:03:43 +0700

    2 lesson microtask (topCarsHomework)

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,26 +1,53 @@
 import React from "react";
 
-type NewComponentType = {
-    students: Array<StudentType>
+
+type NewComponentType ={
+    topCars: Array<topCarsType>
 }
 
-type StudentType = {
-    id: number,
-    name: string,
-    age: number
+type topCarsType = {
+    manufacturer:string,
+    model:string
 }
 
-export const NewComponent = (props: NewComponentType) => {
+export const NewComponent = (props:NewComponentType) => {
     return (
-        <ul>
-            {props.students.map((objectFromStudentArray:StudentType,index)=>{
+        <table>
+            {props.topCars.map((objectFromtopCarstArray:topCarsType,index)=> {
                 return (
-                    <li key={objectFromStudentArray.id}>
-                        <span>{objectFromStudentArray.name}</span>
-                        <span>age: {objectFromStudentArray.age}</span>
-                    </li>
-                    )
+                    <tr>
+                        <th>{index+1}</th>
+                        <th>{objectFromtopCarstArray.manufacturer}</th>
+                        <th>{objectFromtopCarstArray.model}</th>
+                    </tr>
+                )
             })}
-        </ul>
+        </table>
     );
 };
+
+
+// type NewComponentType = {
+//     students: Array<StudentType>
+// }
+//
+// type StudentType = {
+//     id: number,
+//     name: string,
+//     age: number
+// }
+
+// export const NewComponent = (props: NewComponentType) => {
+//     return (
+//         <ul>
+//             {props.students.map((objectFromStudentArray:StudentType,index)=>{
+//                 return (
+//                     <li key={objectFromStudentArray.id}>
+//                         <span>{objectFromStudentArray.name}</span>
+//                         <span>age: {objectFromStudentArray.age}</span>
+//                     </li>
+//                     )
+//             })}
+//         </ul>
+//     );
+// };
