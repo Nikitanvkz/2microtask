917448276998ef5583c7958a2e692e6664df732e
Nikitanvkz <nikitasharafutdinov@yandex.ru>
2022-05-27 03:30:54 +0700

    3 lesson microtask Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,26 @@
-import React  from 'react';
+import React from 'react';
 import './App.css';
-import {NewComponent} from "./NewComponent";
+import {Button} from "./components/Button";
+
 
 function App() {
-    const topCars = [
-        {manufacturer:"BMW",model:'m5cs'},
-        {manufacturer:"Mercedes",model:'e63s'},
-        {manufacturer:"Audi",model:'rs6'}
-    ];
+    const Button1Foo = (subscriber:string,age:number) => {
+        console.log(subscriber,age)
+    }
+    const Button2Foo = (subscriber:string,age:number) => {
+        console.log(subscriber,age)
+    }
+    const StupidButton = () => {
+        console.log('I am stupid Button')
+    }
+
     return (
-        <NewComponent topCars={topCars}/>
-    )
+        <div className="App">
+            <Button name={'MyYouTubeChannel-1'} callBack={()=>(Button1Foo('Vasya',21))}/>
+            <Button name={'MyYouTubeChannel-2'} callBack={()=>(Button2Foo('Ivan',32))}/>
+            <Button name={'Stupid Button'} callBack={StupidButton}/>
+        </div>
+    );
 }
 
 export default App;
-
-// import {NewComponent} from "./NewComponent";
-
-// function App() {
-//     const students = [
-//         {id: 1, name: "James", age: 8},
-//         {id: 2, name: "Robert", age: 18},
-//         {id: 3, name: "John", age: 28},
-//         {id: 4, name: "Michael", age: 38},
-//         {id: 5, name: "William", age: 48},
-//         {id: 6, name: "David", age: 58},
-//         {id: 7, name: "Richard", age: 68},
-//         {id: 8, name: "Joseph", age: 78},
-//         {id: 9, name: "Thomas", age: 88},
-//         {id: 10, name: "Charles", age: 98},
-//         {id: 11, name: "Christopher", age: 100},
-//     ];
-//     return (
-//         <NewComponent students={students}/>
-//     );
-// }
-
